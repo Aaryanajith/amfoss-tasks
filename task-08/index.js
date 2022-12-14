3209ad4d39c94c04dc1dbecc450d150a240d254d
Aaryanajith <aaryanajith45@gmail.com>
2022-12-14 20:13:07 +0530

    Files Added

---
+++ task-08/index.js
@@ -0,0 +1,13 @@
+let crash=function(){document.getElementById("Crash").play()};
+
+let kickbass=function(){document.getElementById("kick-bass").play()};
+
+let snare=function(){document.getElementById("snare").play()};
+
+let tom1=function(){document.getElementById("tom-1").play()};
+
+let tom2=function(){document.getElementById("tom-2").play()};
+
+let tom3=function(){document.getElementById("tom-3").play()};
+
+let tom4=function(){document.getElementById("tom-4").play()};
